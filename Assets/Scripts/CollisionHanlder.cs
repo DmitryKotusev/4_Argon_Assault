835d7b3132a0a94513dcc7bcfb5582739d0fc66e
DmitryKotusev <dssa-1966@yandex.ru>
2019-06-28 13:22:20 +0300

    Added singleton for music player.

--- Assets/Scripts/CollisionHanlder.cs
+++ Assets/Scripts/CollisionHanlder.cs
@@ -2,9 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHanlder : MonoBehaviour
 {
+    [Tooltip("In seconds")]
+    [SerializeField]
+    float levelLoadDelay = 2f;
+    [SerializeField]
+    GameObject deathFX;
+
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
@@ -12,6 +19,13 @@ public class CollisionHanlder : MonoBehaviour
 
     private void StartDeathSequence()
     {
+        deathFX.SetActive(true);
         SendMessage("OnPlayerDeath");
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 }
