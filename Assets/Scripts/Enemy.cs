9908cfd4b29dd522ca8603d93f7daaa986b5c16e
DmitryKotusev <dssa-1966@yandex.ru>
2019-06-28 14:05:26 +0300

    Collision issue needs to be fixed.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided.");
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Auch.");
+    }
+}
