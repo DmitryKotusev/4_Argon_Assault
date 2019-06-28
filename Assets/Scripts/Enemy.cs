0099fcd3ec15de043f0ef5ec53b100542ea62880
DmitryKotusev <dssa-1966@yandex.ru>
2019-06-28 17:25:27 +0300

    Fixed collision bug.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,25 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collided.");
-    }
-
     private void OnParticleCollision(GameObject other)
     {
-        Debug.Log("Auch.");
+        Destroy(gameObject);
     }
 }
