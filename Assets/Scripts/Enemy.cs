9550dacde15121c9a1e24a937c38529b238dd44b
DmitryKotusev <dssa-1966@yandex.ru>
2019-06-29 17:53:52 +0300

    Added instatiating.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    GameObject deathFX;
+
+    private void Start()
+    {
+        AddNoneTriggerBoxCollider();
+    }
+
+    private void AddNoneTriggerBoxCollider()
+    {
+        Collider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
+        GameObject deathExplode = Instantiate(deathFX, transform.position, transform.rotation);
+        
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour
+{
+    [SerializeField]
+    float lifeTime = 3f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
